bce9b86742a2365a7622745857b501cecb4b41cd
Tobias Jordan <80262269+e11902127@users.noreply.github.com>
2025-10-10 16:24:56 +0200

    added Scala hierarchies

--- Scala/Multilevel.scala
+++ Scala/Multilevel.scala
@@ -1,9 +1,17 @@
 class Animal {
     def name: String = "Animal"
+
+    def eat(): Unit = {
+        println(name + " is eating...")
+    }
 }
 
 class Mammal extends Animal {
     override def name: String = "Mammal"
+
+    def shed(): Unit = {
+        println("Shedding fur...")
+    }
 }
 
 class Cat extends Mammal {
@@ -12,4 +20,21 @@ class Cat extends Mammal {
     def meow(): Unit = {
         println("Meow!")
     }
+}
+
+@main def main(): Unit = {
+    val animal = new Animal
+    println(animal.name) // Output: Animal
+    animal.eat() // Output: Animal is eating...
+
+    val mammal = new Mammal
+    println(mammal.name) // Output: Mammal
+    mammal.eat() // Output: Mammal is eating...
+    mammal.shed() // Output: Shedding fur...
+
+    val cat = new Cat
+    println(cat.name) // Output: Cat
+    cat.eat() // Output: Cat is eating...
+    cat.shed() // Output: Shedding fur...
+    cat.meow() // Output: Meow!
 }
