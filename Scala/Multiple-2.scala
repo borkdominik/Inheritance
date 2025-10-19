e32514c1d190467dfbce276ba4b7b3e755fb6812
Dominik Bork <dominik.bork@tuwien.ac.at>
2025-10-19 20:08:10 +0200

    added two more multiple examples to highlight sequencing

---
+++ Scala/Multiple-2.scala
@@ -0,0 +1,31 @@
+trait Pet(){
+    def name: String = "Pet"
+    def pet(): Unit {
+        println(name + " is a pet.")
+    }
+}
+
+trait Animal {
+    def name: String = "Animal"
+    def eat(): Unit = {
+        println(name + " is eating...")
+    }
+    override def pet(): Unit {
+        println(name + " is an animal.")
+    }
+}
+
+class Cat extends Animal with Pet {
+    override def name: String = "Cat"
+    def meow(): Unit = {
+        println("Meow!")
+    }
+}
+
+@main def main(): Unit = {
+    val cat = new Cat
+    println(cat.name) // Output: Cat
+    cat.eat() // Output: Cat is eating...
+    cat.meow() // Output: Meow!
+    cat.pet() // Output: Cat is a pet..
+}
